0860a3977f8233a350b41d6482687c43ac4388b2
yinguoqiang <yinguoqiang@yinguoqiangdeMacBook-Pro.local>
2018-01-08 10:07:34 +0800

    xxx

---
+++ testxxx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testxxx
+//
+//  Created by yinguoqiang on 2018/1/8.
+//  Copyright © 2018年 yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testxxx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testxxx
+//
+//  Created by yinguoqiang on 2018/1/8.
+//  Copyright © 2018年 yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
